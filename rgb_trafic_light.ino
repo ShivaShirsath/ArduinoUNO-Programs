ce8665f280b4d0f9167d075f2fd867cf5f4d86a9
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2022-01-25 22:02:07 +0530

    Create rgb_trafic_light.ino

---
+++ rgb_trafic_light.ino
@@ -0,0 +1,25 @@
+// Coded by Shiva
+int
+  R = 13,
+  G = 12,
+  B = 11
+;
+
+void setup(){
+  pinMode(R, OUTPUT);
+  pinMode(G, OUTPUT);
+  pinMode(B, OUTPUT);
+}
+
+void loop(){
+  signal(HIGH, LOW, LOW);
+  signal(HIGH, HIGH, LOW);
+  signal(LOW, HIGH, LOW);
+}
+
+void signal(int r, int g, int b){
+  digitalWrite(R, r);
+  digitalWrite(G, g);
+  digitalWrite(B, b);
+  delay(r==g?2000:5000);
+}
